f1f5c68465de0a27ab4fb828d45e85cf885605ad
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-23 15:46:34 +1200

    style form

--- src/pages/signInForm.tsx
+++ src/pages/signInForm.tsx
@@ -8,13 +8,13 @@ import { trpc } from '../utils/trpc'
 const SignIn: NextPage = () => {
   const [form, setForm] = useState<FormType>({ name: '', type: 'student' })
   const { query } = useRouter()
-  
+
   const mutation = trpc.useMutation('students.attend')
 
   useEffect(() => {
     const storage = localStorage.getItem('signIn')
     if (storage) {
-      setForm(JSON.parse(storage))
+      // setForm(JSON.parse(storage))
     }
   }, [])
 
@@ -40,24 +40,14 @@ const SignIn: NextPage = () => {
   }
 
   return (
-    <main className="container mx-auto flex flex-col items-center justify-center mt-10  p-4">
+    <main className="w-full max-w-md bg-slate-200">
       <form
-        className=" flex flex-col gap-4 p-8"
+        className="container min-h-screen flex flex-col justify-evenly gap-4 p-8 shadow-md rounded"
         onSubmit={handleSubmit}
       >
-        <fieldset className="text-center flex flex-col gap-1">
-          <label htmlFor="name">Enter Name</label>
-          <input
-            type="text"
-            name="name"
-            defaultValue="Ahmad"
-            className="w-3/4 p-2 mx-auto text-xl text-center rounded-md border-solid border-2 border-slate-200"
-            onChange={handleNameChange}
-            value={form.name}
-          />
-        </fieldset>
-        <fieldset className="text-center flex flex-col">
-          <div className="mx-auto">
+        <p className="text-center tracking-widest">Sign-in Form</p>
+        <fieldset className="text-center flex flex-col items-start mx-auto">
+          <div className="flex items-baseline">
             <input
               type="radio"
               name="type"
@@ -65,12 +55,13 @@ const SignIn: NextPage = () => {
               checked={form.type === 'student'}
               value="student"
               onChange={handlePersonTypeChange}
+              className="checked:text-softblue"
             />
-            <label htmlFor="student" className="mx-2">
+            <label htmlFor="student" className="mx-2 text-2xl">
               Student
             </label>
           </div>
-          <div className="mx-auto">
+          <div className="flex items-baseline">
             <input
               type="radio"
               name="type"
@@ -78,14 +69,25 @@ const SignIn: NextPage = () => {
               checked={form.type === 'guest'}
               value="guest"
               onChange={handlePersonTypeChange}
+              className="checked:text-softblue"
             />
-            <label htmlFor="guest" className="mx-2">
+            <label htmlFor="guest" className="mx-2 text-2xl">
               Non-student
             </label>
           </div>
         </fieldset>
+        <fieldset className="text-center flex flex-col gap-1">
+          <input
+            type="text"
+            name="name"
+            className="w-3/4 p-2 mx-auto text-2xl font-bold text-center rounded-md border-solid border-2 border-slate-200 focus:outline-none focus:outline-1 focus:outline-yellow"
+            onChange={handleNameChange}
+            value={form.name}
+            placeholder="enter your name"
+          />
+        </fieldset>
 
-        <button className="mx-auto my-4 text-white bg-blue-600 rounded-lg p-2 px-4">
+        <button className="mx-auto my-4 w-3/4 h-24 text-white text-2xl bg-ultraviolet rounded-3xl">
           Sign-In
         </button>
       </form>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ultraviolet: '#40276E',
+        softblue: '#9CC1F4',
+        yellow: '#FEC02D',
+        coral: '#EF3A58',
+        turquoise: '#5FC6CB',
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: [require('@tailwindcss/forms')],
+}
