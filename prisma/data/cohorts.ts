b757bb001b2bdd7176e71cf92b912a32738256bd
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-23 18:06:05 +1200

    make an array of objects

--- prisma/data/cohorts.ts
+++ prisma/data/cohorts.ts
@@ -1,3 +1,9 @@
-const cohorts = ['Harakeke', 'Kahikatea', 'Matai', 'Pohutukawa', 'Horoeka']
+const cohorts = [
+  { id: 1, name: 'Harakeke' },
+  { id: 2, name: 'Kahikatea' },
+  { id: 3, name: 'Matai' },
+  { id: 4, name: 'Pohutukawa' },
+  { id: 5, name: 'Horoeka' },
+]
 
 export default cohorts
