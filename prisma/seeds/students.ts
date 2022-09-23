3c6ea8c23ae4151887f85f87087a45dacb9525ef
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-23 18:08:32 +1200

    update seed

--- prisma/seeds/students.ts
+++ prisma/seeds/students.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from '@prisma/client'
 import students from '../data/students'
+import cohorts from '../data/cohorts'
 
 const prisma = new PrismaClient()
 
 const load = async () => {
   try {
+    await prisma.attendances.deleteMany()
     await prisma.students.deleteMany()
+    await prisma.cohorts.deleteMany()
+
+    cohorts.forEach(async (cohort) => {
+      await prisma.cohorts.create({
+        data: cohort,
+      })
+    })
 
     students.forEach(async (student) => {
       await prisma.students.create({
