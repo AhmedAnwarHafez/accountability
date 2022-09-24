0fd0239878c730bf90157790510ed4c773f2a391
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-24 12:35:29 +1200

    update logic for insert attendances

--- src/server/router/students.ts
+++ src/server/router/students.ts
@@ -20,35 +20,42 @@ export const studentsRouter = createRouter()
   })
   .mutation('attend', {
     input: z.object({
-      visitor_id: z.number().nullish(),
       name: z.string(),
-      cohort: z.number().nullish(),
+      cohort: z.number(),
       token: z.string(),
     }),
     async resolve({ input }) {
-      if (!input.visitor_id) {
-        const student = await prisma.students.findFirstOrThrow({
-          where: {
-            AND: [
-              { name: { contains: input.name || undefined } },
-              { cohort_id: input.cohort || undefined },
-            ],
+      const existingStudent = await prisma.students.findFirst({
+        where: {
+          cohort_id: input.cohort,
+          name: input.name,
+        },
+      })
+
+      if (existingStudent) {
+        // student exists in db, a new attendance will be created then exit function
+        await prisma.attendances.create({
+          data: {
+            student_id: existingStudent.id,
+          },
+        })
+        return existingStudent.id
+      } else {
+        const studentId = await prisma.students.create({
+          data: {
+            name: input.name,
+            cohort_id: input.cohort,
+            year: new Date().getFullYear(),
           },
         })
 
         await prisma.attendances.create({
           data: {
-            student_id: student.id,
+            student_id: studentId.id,
           },
         })
 
-        return student
-      } else {
-        return await prisma.attendances.create({
-          data: {
-            student_id: input.visitor_id,
-          },
-        })
+        return studentId
       }
     },
   })
