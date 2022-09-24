58d9fa3842c8e34b121dea3c92b1b558a9f7363b
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-24 13:03:07 +1200

    style component

--- src/pages/thankYou.tsx
+++ src/pages/thankYou.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next'
 
 const thankYou: NextPage = () => {
-  return <p>Thank You</p>
+  return (
+    <main className="flex flex-col gap-4 fixed items-center justify-center w-full h-full bg-ultraviolet text-white font-semibold text-3xl">
+      <p>You are signed in</p>
+      <p>Thank You</p>
+    </main>
+  )
 }
 
 export default thankYou
