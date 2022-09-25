25161e4ed78ec714205e00b08ba1b50af15c22d0
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-26 07:26:57 +1300

    change background color

--- src/pages/thankYou.tsx
+++ src/pages/thankYou.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const thankYou: NextPage = () => {
   return (
-    <main className="flex flex-col gap-4 fixed items-center justify-center w-full h-full bg-ultraviolet text-white font-semibold text-3xl">
+    <main className="flex flex-col gap-4 fixed items-center justify-center w-full h-full bg-softblue text-ultraviolet font-semibold text-3xl tracking-widest">
       <p>You are signed in</p>
       <p>Thank You</p>
     </main>
