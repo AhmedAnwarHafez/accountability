4154893e3c243a2ddaf8ad99dae08e1d2a75a1ff
ahmad <eng.ahmed.anwar@gmail.com>
2022-09-27 06:20:54 +1300

    style dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 import { trpc } from '../utils/trpc'
 import Cohorts from './components/Cohorts'
+import Attendance from './components/Student'
 
 const date = new Date(Date.now() - 5 * 60 * 60 * 1000)
 
@@ -18,21 +19,32 @@ const Dashboard: NextPage = () => {
   const handleCohort = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setCohort(+e.target.value)
   }
+
+  const attendedStudents = attendanceQuery.data?.filter(
+    (student) => Number(student.attended) === 1
+  )
+  const notAttendedStudents = attendanceQuery.data?.filter(
+    (student) => Number(student.attended) === 0
+  )
+
   return (
-    <main className="flex flex-col gap-4 fixed items-center justify-center w-full h-full bg-softblue text-ultraviolet font-semibold text-3xl tracking-widest">
-      {cohortsQuery.data && (
-        <Cohorts cohorts={cohortsQuery.data} handleCohort={handleCohort} />
-      )}
-      <ul>
-        {!attendanceQuery.isLoading &&
-          attendanceQuery.data?.map((student) => (
-            <li key={student.id}>
-              <p>{student.name}</p>
-              <p>{student.inTheSpace}</p>
-            </li>
-          ))}
-      </ul>
-    </main>
+    <div className='h-screen bg-softblue'>
+      <main className="flex flex-col gap-4 items-center justify-center text-xl">
+        <section className="mt-6">
+          {cohortsQuery.data && (
+            <Cohorts cohorts={cohortsQuery.data} handleCohort={handleCohort} />
+          )}
+        </section>
+        <p>({notAttendedStudents?.length}) Not Attended</p>
+        {notAttendedStudents && (
+          <Attendance attendances={notAttendedStudents} style="text-yellow" />
+        )}
+        <p>({attendedStudents?.length}) Attended</p>
+        {attendedStudents && (
+          <Attendance attendances={attendedStudents} style="text-slate-300" />
+        )}
+      </main>
+    </div>
   )
 }
 
